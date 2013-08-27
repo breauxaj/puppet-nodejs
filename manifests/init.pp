6c989619deedf4181ba5472e9f1445eb16cb8b85
Adam Breaux <breauxaj@gmail.com>
2013-08-27 13:23:47 -0400

    instance creation and init script

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,3 +6,29 @@ class nodejs {
   package { $required: ensure => latest }
 
 }
+
+define nodejs::instance ( $user = 'deploy',
+                          $group = 'deploy',
+                          $path = '/var/nodes' ) {
+  file { "$path":
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0775',
+  }
+
+  file { "$path/$name":
+    ensure => directory,
+    owner  => $user,
+    group  => $group,
+    mode   => '0775',
+  }
+
+  file { "/etc/init.d/nodejs-$name":
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template('nodejs/nodejs.erb'),
+  }
+
+}
