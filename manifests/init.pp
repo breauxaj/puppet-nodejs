00efdd590d564e66689b4bd93c7c7a2c0c4748fc
Adam Breaux <breauxa@email.chop.edu>
2015-04-17 09:16:08 -0400

    revamp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,38 @@
-class nodejs {
+# Class: nodejs
+#
+# This class installs the nodejs package
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the nodejs package
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'nodejs':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'nodejs':
+#      ensure => 'absent'
+#    }
+#
+class nodejs (
+  $ensure = 'latest'
+){
   $required = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'nodejs',
   }
 
-  package { $required: ensure => latest }
+  package { $required: ensure => $ensure }
 
 }
